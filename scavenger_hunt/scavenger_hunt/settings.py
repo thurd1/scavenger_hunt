f1baa560b3ca674ec7b58900201123477827a3bf
Trenton <97726179+thurd1@users.noreply.github.com>
2025-02-23 12:43:57 -1000

    Update settings.py

--- scavenger_hunt/scavenger_hunt/settings.py
+++ scavenger_hunt/scavenger_hunt/settings.py
@@ -53,8 +53,12 @@ WSGI_APPLICATION = 'scavenger_hunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your-db-instance-name.region.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
