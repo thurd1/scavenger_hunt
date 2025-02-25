f4498d169b095736f8b2d043e6452c28eee0321d
Trenton <97726179+thurd1@users.noreply.github.com>
2025-02-25 10:20:34 -1000

    Update asgi.py

--- scavenger_hunt/scavenger_hunt/asgi.py
+++ scavenger_hunt/scavenger_hunt/asgi.py
@@ -11,13 +11,14 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.sessions import SessionMiddlewareStack
 from hunt.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scavenger_hunt.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": SessionMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
         )
