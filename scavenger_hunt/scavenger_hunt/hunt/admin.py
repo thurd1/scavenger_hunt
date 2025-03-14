d13c7671c64bdd4675bd94275689d2b0c0ed5dd0
Trenton <97726179+thurd1@users.noreply.github.com>
2025-03-14 09:55:10 -1000

    Delete scavenger_hunt/scavenger_hunt directory

--- scavenger_hunt/scavenger_hunt/hunt/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from hunt.models import Team, Riddle, Clue, GameSession, CustomUser
-
-admin.site.register(Team)
-admin.site.register(Riddle)
-admin.site.register(Clue)
-admin.site.register(GameSession)
-admin.site.register(CustomUser)
--- scavenger_hunt/scavenger_hunt/hunt/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HuntConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hunt'
