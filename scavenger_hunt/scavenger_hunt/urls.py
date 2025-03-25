53af0a98d3202b96aba87ffad57fb8853b726688
Trenton <thurd1@my.hpu.edu>
2025-03-25 10:51:51 -1000

    fixes

--- scavenger_hunt/scavenger_hunt/urls.py
+++ scavenger_hunt/scavenger_hunt/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from scavenger_hunt import views
+from hunt import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
