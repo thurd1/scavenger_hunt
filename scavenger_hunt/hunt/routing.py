467504ed59ca8688023181f1ca62b9ff7ae5af1a
Trenton <thurd1@my.hpu.edu>
2025-03-27 12:27:19 -1000

    race-websocket fixes

--- scavenger_hunt/hunt/routing.py
+++ scavenger_hunt/hunt/routing.py
@@ -5,4 +5,5 @@ websocket_urlpatterns = [
     re_path(r'ws/team/(?P<team_id>\d+)/$', consumers.TeamConsumer.as_asgi()),
     re_path(r'ws/lobby/(?P<lobby_id>\d+)/$', consumers.LobbyConsumer.as_asgi()),
     re_path(r'ws/available-teams/$', consumers.AvailableTeamsConsumer.as_asgi()),
+    re_path(r'ws/race/(?P<race_id>\d+)/$', consumers.RaceConsumer.as_asgi()),
 ] 
