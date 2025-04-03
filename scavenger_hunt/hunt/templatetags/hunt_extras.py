89dcd7b4c241d37f0b0433a4f73f238ce608e15d
Trenton <thurd1@my.hpu.edu>
2025-04-02 14:17:38 -1000

    asdasda

--- scavenger_hunt/hunt/templatetags/hunt_extras.py
+++ scavenger_hunt/hunt/templatetags/hunt_extras.py
@@ -9,3 +9,19 @@ def get_item(dictionary, key):
     Usage in template: {{ my_dict|get_item:key_variable }}
     """
     return dictionary.get(key, []) 
+
+@register.filter
+def filter_by(items, field_name):
+    """
+    Filter a list of items by a field name to create a dictionary
+    where the keys are the values of the specified field.
+    
+    Usage in template: {{ my_list|filter_by:'field_name'|get:value }}
+    """
+    result = {}
+    for item in items:
+        # Get the value of the field for this item
+        value = getattr(item, field_name, None)
+        if value is not None:
+            result[value] = item
+    return result 
