c23d88af97727a3f3d6d06caf8084aafa71d9163
Trenton <97726179+thurd1@users.noreply.github.com>
2025-04-04 12:32:51 -1000

    Add files via upload

---
+++ scavenger_hunt/scavenger_hunt/routing.py
@@ -0,0 +1,22 @@
+from django.urls import re_path
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from hunt.consumers import TeamConsumer, AvailableTeamsConsumer, LobbyConsumer, RaceConsumer
+from django.core.asgi import get_asgi_application
+
+websocket_urlpatterns = [
+    re_path(r'^ws/team/(?P<team_id>\d+)/?$', TeamConsumer.as_asgi()),
+    re_path(r'^ws/available-teams/?$', AvailableTeamsConsumer.as_asgi()),
+    re_path(r'^ws/lobby/(?P<lobby_id>\d+)/?$', LobbyConsumer.as_asgi()),
+    re_path(r'^ws/race/(?P<race_id>\d+)/?$', RaceConsumer.as_asgi()),
+]
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
+    ),
+}) 
