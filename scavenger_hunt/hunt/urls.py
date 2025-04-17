7bdf02d8e1edda4f95d96a844b20360c98b53292
Trenton <thurd1@my.hpu.edu>
2025-04-16 14:37:50 -1000

    fix

--- scavenger_hunt/hunt/urls.py
+++ scavenger_hunt/hunt/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from hunt import views
 
 urlpatterns = [
     path('', views.home, name='home'),
